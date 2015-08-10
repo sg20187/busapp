5c483432a4664b9bfbb096c9bbb3d9ccbb17c7ba
gsatej <satej_gs@yahoo.co.in>
2015-08-10 22:46:09 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ module.exports = {
 
 	// the database url to connect
 	//url : 'mongodb://localhost/test'
-	url : 'mongodb://127.0.0.1:27017/test'
+	//url : 'mongodb://127.0.0.1:27017/test'
+	url : 'mongodb://devuser:remotedev@ec2-54-148-74-187.us-west-2.compute.amazonaws.com:27017/dummyDB'
 }
